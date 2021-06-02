c6ee55b4bdf7a215c4e64a9c4b9aa8c455f6f055
Ryan <ryan.mossor@gmail.com>
2021-06-01 20:49:26 -0500

    Completed chapter 12.2

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,35 +16,27 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<Event> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        /* List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events); */
-
-        HashMap<String, String> events = new HashMap<>();
-        events.put("WWDC", "an information technology conference held annually by Apple Inc.");
-        events.put("Google I/O", "an annual developer conference held by Google in Mountain View, California");
-        events.put("DockerCon", "a free, one day virtual event for development teams who are building the next " +
-                "generation of modern applications");
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     // lives at /events/create -- okay to host at same location
     // as above since these two methods handle different types of requests
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-//        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.add(new Event(eventName, eventDescription));
         return "redirect:";     // redirect to the root path for this controller
     }
 
